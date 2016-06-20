e429e2f65b05f2eff0dfa3a7b7ebb03e5a84cb7a
somethingsaid <kwong.billy@gmail.com>
2016-06-20 10:59:04 -0400

    Implemented click to pause, dblclick reset

---
+++ app/megavideoApp.js
@@ -0,0 +1,53 @@
+angular.module('megaVideoDemo', []).
+	directive('megaVideo', function($sce) {
+		return {
+			restrict: 'E',
+			templateUrl: 'mega-video.html',
+			scope: true,
+			link: function(scope, element, attrs) {
+				var videoPlayer = element.find('video')[0];
+				scope.sources = [];
+				
+				// whitelist of video formats accepted
+				function processSources() {
+					var sourceTypes = {
+						webm: { type: 'video/webm'},
+						mp4: { type: 'video/mp4'},
+						ogg: { type: 'video/ogg'}
+						// etc...
+					}
+					for (source in sourceTypes) {
+						if (attrs.hasOwnProperty(source)) {
+							scope.sources.push(
+								{ type: sourceTypes[source].type, 
+								  src: $sce.trustAsResourceUrl(attrs[source])
+								}
+							);
+						}
+					}
+				}
+				processSources();
+				scope.video =  {
+			        play: function() {
+			          videoPlayer.play();
+			          scope.video.status = 'play';
+			        },
+			        pause: function() {
+			          videoPlayer.pause();
+			          scope.video.status = 'pause';
+			        },
+			        stop: function() {
+			        	this.pause();
+			        	videoPlayer.currentTime = 0;
+			        	scope.video.status = 'stop';
+			        },
+			        togglePlay: function() {
+			          scope.video.status == 'play' ? this.pause() : this.play();
+			        },
+			        width: attrs.width,
+			        height: attrs.height
+			    };
+			}
+			
+		}
+	});
